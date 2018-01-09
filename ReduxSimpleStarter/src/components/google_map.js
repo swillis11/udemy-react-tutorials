656eee62a8e9f7e9b76755ad8e11a4d308b96937
Sean Willis <seanwillis@Seans-MacBook-Pro.local>
2018-01-09 15:37:39 -0800

    Adding react-google-maps

--- ReduxSimpleStarter/src/components/google_map.js
+++ ReduxSimpleStarter/src/components/google_map.js
@@ -1,20 +1,32 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
 //Google maps js is added in index.html
-class GoogleMap extends Component {
-    componentDidMount() { //After render
-        new google.maps.Map(this.refs.map, {
-            zoom: 12,
-            center: {
-                lat: this.props.lat,
-                lng: this.props.lon
-            }
-        })
-    }
+// class GoogleMap extends Component {
+//     componentDidMount() { //After render
+//         new google.maps.Map(this.refs.map, {
+//             zoom: 12,
+//             center: {
+//                 lat: this.props.lat,
+//                 lng: this.props.lon
+//             }
+//         })
+//     }
 
-    render() {
-        return <div ref="map" />;
-    }
+//     render() {
+//         return <div ref="map" />;
+//     }
+// }
+
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+export default (props) => {
+    <GoogleMapLoader
+        containerElement={ <div style={{height: '100%' }} /> }
+        googleMapElement={
+            <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+        }
+        />
 }
 
 export default GoogleMap;
